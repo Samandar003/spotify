1c595374ce9a333a7e161fdf789ebacc731e1e11
samandar <samandar200527@gmail.com>
2023-06-08 22:16:34 +0500

    requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'musicdb',
-        'USER':"postgres",
-        'PASSWORD': '4Yillikarmiya',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER':os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
